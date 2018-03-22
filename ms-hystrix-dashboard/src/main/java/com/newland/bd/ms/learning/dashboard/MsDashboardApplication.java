6d41f3be5362bd3a104f0c44f6aff8059ddcf4de
liangchangsheng <lingyoucao@126.com>
2018-03-22 13:42:24 +0800

    修改

--- ms-hystrix-dashboard/src/main/java/com/newland/bd/ms/learning/dashboard/MsDashboardApplication.java
+++ ms-hystrix-dashboard/src/main/java/com/newland/bd/ms/learning/dashboard/MsDashboardApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
-import org.springframework.cloud.netflix.turbine.TurbineClustersProvider;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @EnableHystrixDashboard
